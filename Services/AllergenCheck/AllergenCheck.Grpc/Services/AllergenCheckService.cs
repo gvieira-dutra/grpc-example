57ad32863f6f96093e58781c6a64548cb5004dbe
gvieira-dutra <gleisonvieiraa@gmail.com>
2024-11-12 19:25:17 -0500

    Create, update and delete allergen tasks added to microservice

--- Services/AllergenCheck/AllergenCheck.Grpc/Services/AllergenCheckService.cs
+++ Services/AllergenCheck/AllergenCheck.Grpc/Services/AllergenCheckService.cs
@@ -1,4 +1,5 @@
 ﻿using AllergenService.Grpc;
+using Google.Protobuf;
 
 namespace AllergenCheck.Grpc.Services;
 
@@ -17,7 +18,7 @@ public class AllergenCheckService(AllergenCheckContext dbContext, ILogger<Allerg
         if (allergenInfo is null)
         {
             logger.LogInformation(
-                "Ingredient not found on database");
+                "Ingredient not found on database: {ingredientName}", request.IngredientName);
 
             return new AllergenModel
             {
@@ -28,8 +29,90 @@ public class AllergenCheckService(AllergenCheckContext dbContext, ILogger<Allerg
         }
 
         logger.LogInformation(
-            "Found ingredient: {IngredientName}", allergenInfo.IngredientName);
+            "Found allergen ingredient: {IngredientName}", allergenInfo.IngredientName);
 
         return allergenInfo.ToModel();
     }
+
+    public override async Task<AllergenModel> CreateAllergen(
+    CreateAllergenRequest request,
+    ServerCallContext context)
+    {
+        var allergen = request.Allergen.ToEntity();
+
+        dbContext.Add(allergen);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation(
+            "Success! New allergen info added to database."
+            );
+
+        return allergen.ToModel();
+
+    }
+
+    public override async Task<AllergenModel> UpdateAllergen(
+UpdateAllergenRequest request,
+ServerCallContext context)
+    {
+        var allergen = await dbContext.Allergens
+            .FirstOrDefaultAsync(a => a.IngredientName == request.Allergen.IngredientName);
+
+        if(allergen is null)
+        {
+            logger.LogInformation(
+                "Product not found on database: {productName}!", request.Allergen.IngredientName
+                );
+
+            throw new RpcException(
+                new Status(
+                StatusCode.NotFound,
+                $"Could not find ingredient: {request.Allergen.IngredientName}")
+                );         
+        }
+
+            allergen.Descrip = request.Allergen.Descrip;
+            allergen.IngredientName = request.Allergen.IngredientName;
+            allergen.SeverityLevel = request.Allergen.SeverityLevel;
+
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation(
+                "Successfully updated information for: {ingredientName}!", allergen.IngredientName
+                );
+
+            return allergen.ToModel();
+    }
+
+    public override async Task<DeleteAllergenResponse> DeleteAllergen(
+DeleteAllergenRequest request,
+ServerCallContext context)
+    {
+        var allergen = await dbContext.Allergens
+            .FirstOrDefaultAsync(a => a.IngredientName == request.Allergen.IngredientName);
+
+        if (allergen is null)
+        {
+            logger.LogInformation(
+                "Product not found on database: {productName}!", request.Allergen.IngredientName
+                );
+
+            throw new RpcException(
+                new Status(
+                StatusCode.NotFound,
+                "An error occurred, please try again later.")
+                );
+        }
+
+        dbContext.Allergens.Remove(allergen);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation(
+                "Successfully deleted: {ingredientName}!", request.Allergen.IngredientName
+                );
+
+        return new DeleteAllergenResponse { Succes = true };
+
+    }
+
 }
